30fd9ee0179ed6ab973602f1761020ada363d3df
marquitobb-rasp <maca9608@gmail.com>
2021-08-21 19:31:03 -0500

    add flow with gpio

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN)
-GPIO.setup(18, GPIO.OUT)
 
-while True:
-    inputValue = GPIO.input(24)
-    if (inputValue == True):
-        GPIO.output(18, GPIO.HIGH)
-        print("Encendido")
-        time.sleep(0.3)
-        break
-while True:
-    inputValue = GPIO.input(24)
-    if (inputValue == True):
-        GPIO.output(18, GPIO.LOW)
-        print("Apagado")
-        time.sleep(0.3)
-        break
-GPIO.cleanup()
+class Alarm:
+    """ "base class to execute alarm on function and notify otherwise"""
+
+    @classmethod
+    def activate_alarm(cls):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(24, GPIO.IN)
+        GPIO.setup(18, GPIO.OUT)
+
+        try:
+            while True:
+                GPIO.output(18, GPIO.HIGH)
+                inputValue = GPIO.input(24)
+                if inputValue == True:
+                    # TODO: send notification for a certain time to say that the alarm is still active
+                    # time.sleep(0.5)
+                    pass
+                else:
+                    cls.send_notification()
+                    GPIO.cleanup()
+                    break
+        except KeyboardInterrupt:
+            print("se cancelo alarma!")
+            GPIO.cleanup()
+
+    @classmethod
+    def send_notification(cls):
+        # TODO: send notification with the telegram app or some email
+        print("Se desactivo alarma x_x")
+        pass
+
+
+if __name__ == "__main__":
+    print("Alarma activate...")
+    Alarm.activate_alarm()
